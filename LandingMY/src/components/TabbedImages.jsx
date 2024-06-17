22bb0340ea6f8e50440c2bd75533474af8e5b099
mlobeto <mercedeslobeto@gmail.com>
2024-06-16 22:02:20 -0300

    responsive

--- LandingMY/src/components/TabbedImages.jsx
+++ LandingMY/src/components/TabbedImages.jsx
@@ -57,54 +57,54 @@ const TabbedImages = () => {
   const { image, text } = getImageAndText(activeTab);
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex space-x-4 mb-4">
-        <button
-          className={`px-4 py-2 rounded-full ${activeTab === 'tab1' ? 'bg-yellow-500 text-white' : 'bg-gray-200 text-gray-600'}`}
-          onClick={() => handleTabClick('tab1')}
-        >
-          Resumen
-        </button>
-        <button
-          className={`px-4 py-2 rounded-full ${activeTab === 'tab2' ? 'bg-yellow-500 text-white' : 'bg-gray-200 text-gray-600'}`}
-          onClick={() => handleTabClick('tab2')}
-        >
-          Facturación
-        </button>
-        <button
-          className={`px-4 py-2 rounded-full ${activeTab === 'tab3' ? 'bg-yellow-500 text-white' : 'bg-gray-200 text-gray-600'}`}
-          onClick={() => handleTabClick('tab3')}
-        >
-          Turnos
-        </button>
-        <button
-          className={`px-4 py-2 rounded-full ${activeTab === 'tab4' ? 'bg-yellow-500 text-white' : 'bg-gray-200 text-gray-600'}`}
-          onClick={() => handleTabClick('tab4')}
-        >
-          Coordenadas
-        </button>
-        <button
-          className={`px-4 py-2 rounded-full ${activeTab === 'tab5' ? 'bg-yellow-500 text-white' : 'bg-gray-200 text-gray-600'}`}
-          onClick={() => handleTabClick('tab5')}
-        >
-          Tareas
-        </button>
-        <button
-          className={`px-4 py-2 rounded-full ${activeTab === 'tab6' ? 'bg-yellow-500 text-white' : 'bg-gray-200 text-gray-600'}`}
-          onClick={() => handleTabClick('tab6')}
-        >
-          Caja
-        </button>
+    <div className="flex flex-col items-center mt-4">
+    <div className="grid grid-cols-3 gap-2 mb-2 md:mb-4 "> {/* Usar grid para distribuir botones */}
+      <button
+        className={`px-2 py-1 rounded md:rounded-md text-xs md:text-sm ${activeTab === 'tab1' ? 'bg-yellow-500 text-white' : 'bg-gray-200 text-gray-600'}`}
+        onClick={() => handleTabClick('tab1')}
+      >
+        Resumen
+      </button>
+      <button
+        className={`px-2 py-1 rounded md:rounded-md text-xs md:text-sm ${activeTab === 'tab2' ? 'bg-yellow-500 text-white' : 'bg-gray-200 text-gray-600'}`}
+        onClick={() => handleTabClick('tab2')}
+      >
+        Facturación
+      </button>
+      <button
+        className={`px-2 py-1 rounded md:rounded-md text-xs md:text-sm ${activeTab === 'tab3' ? 'bg-yellow-500 text-white' : 'bg-gray-200 text-gray-600'}`}
+        onClick={() => handleTabClick('tab3')}
+      >
+        Turnos
+      </button>
+      <button
+        className={`px-2 py-1 rounded md:rounded-md text-xs md:text-sm ${activeTab === 'tab4' ? 'bg-yellow-500 text-white' : 'bg-gray-200 text-gray-600'}`}
+        onClick={() => handleTabClick('tab4')}
+      >
+        Coordenadas
+      </button>
+      <button
+        className={`px-2 py-1 rounded md:rounded-md text-xs md:text-sm ${activeTab === 'tab5' ? 'bg-yellow-500 text-white' : 'bg-gray-200 text-gray-600'}`}
+        onClick={() => handleTabClick('tab5')}
+      >
+        Tareas
+      </button>
+      <button
+        className={`px-2 py-1 rounded md:rounded-md text-xs md:text-sm ${activeTab === 'tab6' ? 'bg-yellow-500 text-white' : 'bg-gray-200 text-gray-600'}`}
+        onClick={() => handleTabClick('tab6')}
+      >
+        Caja
+      </button>
+    </div>
+    <div className="w-full flex flex-col md:flex-row justify-center items-center md:items-start space-y-2 md:space-y-0 md:space-x-4"> {/* Ajuste del espacio entre elementos */}
+      <div className="w-full md:w-1/3 p-2 md:p-4"> {/* Ajuste del padding */}
+        <p className="text-sm md:text-base text-gray-800">{text}</p> {/* Ajuste del tamaño del texto */}
       </div>
-      <div className="w-full flex flex-col md:flex-row justify-center items-center md:items-start space-y-4 md:space-y-0 md:space-x-4">
-        <div className="w-full md:w-1/3 p-4">
-          <p className="text-gray-800">{text}</p>
-        </div>
-        <div className="w-full md:w-2/3 flex justify-center items-center">
-          <img src={image} alt="Tab Image" className="w-full h-auto max-w-full md:max-w-md" />
-        </div>
+      <div className="w-full md:w-2/3 flex justify-center items-center">
+        <img src={image} alt="Tab Image" className="w-full h-auto max-w-full md:max-w-md" />
       </div>
     </div>
+  </div>
   );
 };
 
